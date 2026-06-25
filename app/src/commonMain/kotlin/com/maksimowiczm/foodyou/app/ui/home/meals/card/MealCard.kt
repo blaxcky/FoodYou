6fa72385a8de2baf7e99af30458d97c434c1a9a8
Markus <YOUR_EMAIL>
2026-06-25 21:38:30 +0200

    Align meal card header actions

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/ui/home/meals/card/MealCard.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/ui/home/meals/card/MealCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -165,7 +166,7 @@ internal fun MealCard(
 
                 MealNutritionSummary(
                     meal = meal,
-                    modifier = Modifier.padding(top = 2.dp),
+                    modifier = Modifier.padding(top = 2.dp).widthIn(min = 150.dp),
                 )
             }
 
